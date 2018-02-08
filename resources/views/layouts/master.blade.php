7c2e47b07b5bbc5316b8920efd711c4fbb5b6efb
RafaelMLMacedo <rafaelmacedo4@gmail.com>
2018-02-08 00:14:39 +0000

    Migrated to a Master Blade file

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<noscript>
+    <div id="no-script-warning">This application needs JavaScript enabled in order to run at its best!</div>
+</noscript>
+<div id="app">
+    @include('layouts.partials.navbar')
+    <main class="py-4">
+        @yield('content')
+    </main>
+</div>
+
+<!-- Scripts -->
+<script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>

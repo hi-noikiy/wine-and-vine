eae8ebf3923541720230744aae3a4538398be0f3
RafaelMLMacedo <rafaelmacedo4@gmail.com>
2018-02-09 01:50:09 +0000

    First approach to the user table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,20 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->unsignedInteger('wine_id');                 // used for wishlist purposes
+            $table->string('firstname', 25);
+            $table->string('lastname', 25);
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('description')->nullable();
+            $table->string('country');
+            // TODO: Add a default path for the user avatar
+            $table->string('avatar')->default('');
+            $table->unsignedBigInteger('rating_count')->default(0);
+            $table->enum('rating_visibility', ['private', 'friends', 'public'])->default('public');
+            $table->boolean('newsletter')->default(true);
+            $table->boolean('email_offers')->default(true);
+            $table->unsignedInteger('rank')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });

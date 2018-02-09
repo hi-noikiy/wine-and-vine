771d620016f9e0b39fd525a68571abcbc9b40121
RafaelMLMacedo <rafaelmacedo4@gmail.com>
2018-02-09 18:16:56 +0000

    Added the tests where a user can add multiple wines and remove multiple wines from his wishlist

--- tests/Feature/WishlistTest.php
+++ tests/Feature/WishlistTest.php
@@ -32,6 +32,29 @@ class WishlistTest extends TestCase
     }
 
     /** @test */
+    public function a_user_can_add_multiple_wines_into_his_wishlist() {
+        $user = factory(User::class)->create();
+
+        $wines = factory(Wine::class, 5)->create();
+
+        $wines->each(function ($wine) use ($user) {
+            $this->assertDatabaseMissing('user_wine', [
+                'user_id' => $user->id,
+                'wine_id' => $wine->id
+            ]);
+        });
+
+        $user->wishlist()->attach($wines);
+
+        $wines->each(function ($wine) use ($user) {
+            $this->assertDatabaseHas('user_wine', [
+                'user_id' => $user->id,
+                'wine_id' => $wine->id
+            ]);
+        });
+    }
+
+    /** @test */
     public function a_user_can_remove_a_wine_from_his_wishlist() {
         $user = factory(User::class)->create();
 
@@ -50,5 +73,32 @@ class WishlistTest extends TestCase
             'user_id' => $user->id,
             'wine_id' => $wine->id
         ]);
+    }
+
+    /** @test */
+    public function a_user_can_remove_multiple_wines_from_his_wishlist() {
+        $user = factory(User::class)->create();
+
+        $wines = factory(Wine::class, 5)->create();
+
+        $user->wishlist()->attach($wines);
+
+        $wines->each(function ($wine) use ($user) {
+            $this->assertDatabaseHas('user_wine', [
+                'user_id' => $user->id,
+                'wine_id' => $wine->id
+            ]);
+        });
+
+        $user->wishlist()->detach($wines);
+
+        $wines->each(function ($wine) use ($user) {
+            $this->assertDatabaseMissing('user_wine', [
+                'user_id' => $user->id,
+                'wine_id' => $wine->id
+            ]);
+        });
+
+
     }
 }

ff0fdb65ca0b3e1749c9c1efd45b2f5900cc7ca3
RafaelMLMacedo <rafaelmacedo4@gmail.com>
2018-02-09 18:11:08 +0000

    Added the test where a user can remove a wine from his wishlist

---
+++ tests/Feature/WishlistTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use App\Wine;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class WishlistTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_add_a_wine_into_his_wishlist() {
+        $user = factory(User::class)->create();
+
+        $wine = factory(Wine::class)->create();
+
+        $this->assertDatabaseMissing('user_wine', [
+            'user_id' => $user->id,
+            'wine_id' => $wine->id
+        ]);
+
+        $user->wishlist()->attach($wine);
+
+        $this->assertDatabaseHas('user_wine', [
+            'user_id' => $user->id,
+            'wine_id' => $wine->id
+        ]);
+    }
+
+    /** @test */
+    public function a_user_can_remove_a_wine_from_his_wishlist() {
+        $user = factory(User::class)->create();
+
+        $wine = factory(Wine::class)->create();
+
+        $user->wishlist()->attach($wine);
+
+        $this->assertDatabaseHas('user_wine', [
+            'user_id' => $user->id,
+            'wine_id' => $wine->id
+        ]);
+
+        $user->wishlist()->detach($wine);
+
+        $this->assertDatabaseMissing('user_wine', [
+            'user_id' => $user->id,
+            'wine_id' => $wine->id
+        ]);
+    }
+}

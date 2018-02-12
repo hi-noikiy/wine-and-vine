3c5542a78b7500333369433ee9b72012b2dd5ffe
RafaelMLMacedo <rafaelmacedo4@gmail.com>
2018-02-12 04:32:24 +0000

    Added the migrations for the Address

---
+++ database/migrations/2018_02_12_040858_create_addresses_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('city_id');
+            $table->string('name');
+            $table->string('street');
+            $table->string('post_code', 3);
+            $table->boolean('is_primary');
+            $table->timestamps();
+
+            $table->unique(['street', 'city_id']);
+
+            $table->foreign('city_id')
+                ->references('id')
+                ->on('cities');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}

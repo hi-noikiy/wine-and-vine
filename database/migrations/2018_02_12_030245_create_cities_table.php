31c31b5999f7e4b8e4545ad32ac90d6edab84261
RafaelMLMacedo <rafaelmacedo4@gmail.com>
2018-02-12 03:36:27 +0000

    Added the migrations for the Country, Region and Cities

---
+++ database/migrations/2018_02_12_030245_create_cities_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('postal_code', 4);
+            $table->unsignedInteger('region_id');
+            $table->timestamps();
+
+            $table->unique(['name', 'region_id']);
+
+            $table->foreign('region_id')
+                ->references('id')
+                ->on('regions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}

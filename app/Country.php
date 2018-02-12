f34065771444568f783c6dcebd61da12793503ad
RafaelMLMacedo <rafaelmacedo4@gmail.com>
2018-02-12 05:03:19 +0000

    Added the $fillable property to the Country model, regions function that reflects the Eloquent relation with Region model, accessor and mutator for the Country name and finally, the getRouteKeyName override from the parent

--- app/Country.php
+++ app/Country.php
@@ -6,5 +6,55 @@ use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
-    //
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name'
+    ];
+
+    /**
+     * Fetch Country's Regions
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function regions()
+    {
+        return $this->hasMany(Region::class);
+    }
+
+    /**
+     * Set the Country's name
+     *
+     * @param string $name
+     * @return void
+     */
+    public function setNameAttribute(string $name)
+    {
+        $this->attributes['name'] = strtolower($name);
+    }
+
+    /**
+     * Get the Country's name
+     *
+     * @param string $name
+     * @return string
+     */
+    public function getNameAttribute(string $name)
+    {
+        return ucwords($name);
+    }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'name';
+    }
 }

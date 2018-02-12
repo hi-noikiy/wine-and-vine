5646d3b0e7bb745531194a126d884a72f076a81b
RafaelMLMacedo <rafaelmacedo4@gmail.com>
2018-02-12 01:32:12 +0000

    Created a WineSeeder::class to populate the wines table after a migrate --seed artisan command

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RatingVisibilitiesSeeder::class,
             UserSeeder::class,
+            WineSeeder::class,
         ]);
     }
 }
